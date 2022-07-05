856ddee064c345bda061d9b348fec6376d50bb1e
JaraMohnson <jaramohnson@gmail.com>
2022-07-05 17:46:59 -0400

    First commit for circles lab

---
+++ Circle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirclesLab
+{
+    internal class Circle
+    {
+        public double Radius;
+
+
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+        }
+
+        public double CalculateCircumference()
+        {
+            double circumference = (2 * Math.PI * Radius);           
+            return circumference;
+        }
+
+        public double CalculateArea()
+        {
+            double area = (Math.PI * (Radius * Radius));            
+            return area;
+        }
+        private string FormatNumber(double x)
+        {
+            return Math.Round(x, 2).ToString();
+            //can put in Calc circumference, area, etc 
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            string formattedCircumference = FormatNumber(CalculateCircumference());
+            return formattedCircumference;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = FormatNumber(CalculateArea());
+            return formattedArea;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using CirclesLab;
+
+bool validData = false;
+bool playAgain = true;
+double radius = 0;
+string area = "";
+string circumference = "";
+int count = 0;
+
+Console.WriteLine("Welcome to the circle tester.");  
+
+while (playAgain == true)
+{
+
+    while (!validData)
+    {
+        try
+        {
+            Console.WriteLine("Please enter the radius:");
+            radius = double.Parse(Console.ReadLine());
+            if (radius < 0)
+            {
+                throw new Exception("Number must be positive");
+            }
+            else
+            {
+                validData = true;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            continue;
+        }
+    }
+
+    // do calculations 
+
+    Circle userCircle = new Circle(radius);  //make a new circle 
+
+    area = userCircle.CalculateFormattedArea();
+    circumference = userCircle.CalculateFormattedCircumference();
+    count++;
+    Console.WriteLine($"The area of your circle is {area}");
+    Console.WriteLine($"The circumference of your circle is {circumference}");
+
+    while (true)
+    {
+        Console.WriteLine("Do you want to run again? y/n ");
+        string restart = Console.ReadLine().Trim().ToLower();
+
+        if (restart == "y" || restart == "yes")
+        {
+            validData = false; // gotta get new data now 
+            break;
+        }
+        else if (restart == "n" || restart == "no")
+        {
+            Console.WriteLine($"Goodbye! You calculated {count} circles.");
+            playAgain = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Please make a valid selection");
+            continue;
+        }
+    }
+}
